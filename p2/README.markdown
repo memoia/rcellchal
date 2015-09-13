de02247531f4bec71558e39ed5e09debbdf75f19
Ian Melnick <melnick@gmail.com>
2015-09-12 21:03:50 -0500

    Add threading for continuous monitoring

--- p2/README.markdown
+++ p2/README.markdown
@@ -1,15 +1,11 @@
 Problem 2
 ---------
 
-Requires [virtualenv](https://pypi.python.org/pypi/virtualenv) to be
-installed and available on the PATH. Requires standard build
-tools (i.e., build-essential or XCode), including gcc.
+You can create a symlink to the application with ``make``, but this is
+kind of silly. Executing ``tail.py -h`` (to get help options) should
+work just fine!
 
-Installation:
-
-``make``
-
-Summary:
+### Summary
 
 Write a clone of the tail program available in unix/linux systems but
 only support a very small subset of the flags/options/features it
@@ -17,10 +13,16 @@ supports. Pick and choose the complexity/scope based on the time you
 have and the options you like to support. Support at least the basic
 functionality you get with no options.
 
-Some thoughts:
+### Some thoughts
 
 Things get more complicated when we involve more than one file stream.
 There's a nice cross-platform directory monitoring library called
-[watchdog](https://pythonhosted.org/watchdog/). Let's use that so we
-don't re-invent the wheel and produce more code that would otherwise
-need to be tested.
+[watchdog](https://pythonhosted.org/watchdog/), and in most cases it'd
+be better not to re-invent the wheel. For the purpose of this exercise,
+I want to have a self-contained file that doesn't need external dependencies.
+
+### Bugs
+
+* When monitoring files for changes, if the length doesn't change (but,
+  say, a character changes), the change isn't shown.
+* Standard in monitoring should not be trusted; could really use some work.
--- p2/tail.py
+++ p2/tail.py
@@ -1,40 +1,78 @@
-#!opt/bin/python
+#!/usr/bin/env python2.7
 
+import os
+import sys
 import logging
 import argparse
+import threading
 from time import sleep
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 
 
 logging.basicConfig(level=logging.DEBUG)
 LOG = logging.getLogger(__name__)
 
 
-class FileModifiedHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        print 'yep'
+class FileMonitor(threading.Thread):
+    def __init__(self, path, interval, stop_event):
+        self.path = path
+        self.interval = interval
+        self.stop_event = stop_event
+        self.last_stat = self.stat(self.path)
+        self.offset = self.last_stat.st_size
+        threading.Thread.__init__(self)
+
+    def stat(self, identifier):
+        return (os.fstat(sys.stdin.fileno())
+                if identifier == '-'
+                else os.stat(identifier))
+
+    def next(self):
+        data = None
+        if self.path == '-':
+            data = sys.stdin.read()
+        else:
+            if os.path.isfile(self.path):
+                fd = open(self.path, 'r')
+                fd.seek(self.offset)
+                data = fd.read()
+                self.offset = fd.tell()
+                fd.close()
+        return data
+
+    def run(self):
+        while not self.stop_event.is_set():
+            current_stat = self.stat(self.path)
+            if self.last_stat.st_mtime != current_stat.st_mtime:
+                LOG.debug('Change to {}'.format(self.path))
+                if self.last_stat.st_size < self.offset:
+                    self.offset = 0
+                buff = self.next()
+                if buff:
+                    print buff
+                self.last_stat = current_stat
+            sleep(self.interval)
 
 
-class ObserverCoordinator(object):
+class MonitorCoordinator(object):
     def __init__(self, args):
         self.args = args
-        self.observers = []
-        event = FileModifiedHandler()
+        self.threads = []
+        self.stop_event = threading.Event()
         for path in self.args.filepath:
-            obs = Observer(timeout=self.args.s)
-            obs.schedule(event, path, recursive=False)
-            self.observers.append(obs)
+            self.threads.append(FileMonitor(path, self.args.s, self.stop_event))
         self.start()
 
     def start(self):
-        map(lambda o: o.start(), self.observers)
+        map(lambda t: t.start(), self.threads)
         try:
             while True:
                 sleep(self.args.s)
         except KeyboardInterrupt:
-            map(lambda o: o.stop(), self.observers)
-            map(lambda o: o.join(), self.observers)
+            self.stop()
+
+    def stop(self):
+        self.stop_event.set()
+        map(lambda t: t.join(), self.threads)
 
 
 def main():
@@ -52,7 +90,8 @@ def main():
     parser.add_argument('filepath', nargs='*',
                         default=['-'], help='file to read')
     args = parser.parse_args()
-    observers = ObserverCoordinator(args)
+    if args.f:
+        MonitorCoordinator(args)
 
 
 if __name__ == '__main__':
